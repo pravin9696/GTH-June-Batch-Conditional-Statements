23675a0b44a6a7ddfbf6cd28e152a84b21023919
pravi <pravi@DESKTOP-IRFMLF6>
2024-06-24 11:20:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Conditional_statements_240624
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int roll = 1001;
+            float fees = 15452.34f;
+            string name = "rajesh";
+            char div='a';
+            // student information
+
+            // roll=1001 Name of student = rajesh div= a fees paid=15452.34
+            //1 string concat
+            Console.WriteLine("roll="+roll+" Name of student ="+name+" div= "+div+" fees paid= "+fees);
+            //2 place holder
+            Console.WriteLine("roll= {0} Name of student = {1} ", roll,  name);
+            //3 string interpolation.
+            Console.WriteLine($"roll= {roll} Name of student = {name} ");
+
+        }
+        
+    }
+}
+
