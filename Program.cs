281fef58066d9c3aad5816706b532b1572a6dc56
pravin gaikwad <pravingaikwad.sibar@outlook.com>
2024-06-25 12:08:55 +0530

    conditional Statements if , if else study 250624

--- Program.cs
+++ Program.cs
@@ -1,27 +1,87 @@
-﻿namespace Conditional_statements_240624
+﻿using System.Numerics;
+using System.Threading.Channels;
+
+namespace Conditional_statements_240624
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int roll = 1001;
-            float fees = 15452.34f;
-            string name = "rajesh";
-            char div='a';
-            // student information
+            Console.WriteLine("hello");
+            #region
+            //int x = 5;
+            //int y;
+            //string city = "pune";
+            //string state = "Maha";
+            //string abc=x >= 5 ? city : state;
+            //var xyz = x >= 5 ? 111 : 222;
+            //int ans = x < 10 ? 1000 : 2000;
+            //Console.WriteLine("Ans="+ans);
+            //int no;
 
-            // roll=1001 Name of student = rajesh div= a fees paid=15452.34
-            //1 string concat
-            Console.WriteLine("roll="+roll+" Name of student ="+name+" div= "+div+" fees paid= "+fees);
-            //2 place holder
-            Console.WriteLine("roll= {0} Name of student = {1} ", roll,  name);
-            //3 string interpolation.
-            Console.WriteLine($"roll= {roll} Name of student = {name} ");
-            //////////////////////////
-            Console.WriteLine("sdfasd");
-            Console.WriteLine("sdfasdf");
+            //Console.WriteLine("Enter no:");
+            //no = int.Parse(Console.ReadLine());//34
+            //if (no%2==0)
+            //{
+            //    Console.WriteLine(no+" is even"); // 34 is even
+            //}
+            //else
+            //{
+            //    Console.WriteLine(no+"is odd");
+            //}
+            #endregion
+            #region
+            //int n1, n2;
+
+            ////accept two nos and store into n1 & n2;
+            //Console.WriteLine("Enter n1 & n2");
+            //n1 = int.Parse(Console.ReadLine());//4
+            //n2 = int.Parse(Console.ReadLine());//6
+            //if (n1 > n2)
+            //{
+            //    Console.WriteLine(n1 + " is greater:");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(n2 + " is greater:");
+            //}
+            #endregion
+            #region greater 3 using only if
+            int n1 = 400;
+          int n2 = 555;
+          int n3 = 90;
+
+            //if (n1 > n2 && n1 > n3)
+            //{
+            //    Console.WriteLine(n1 + "is greater number:");
+            //}
+            //if (n2 > n1 && n2 > n3)
+            //{
+            //    Console.WriteLine(n2 + "is greater number:");
+            //}
+            //if (n3 > n1 && n3 > n2)
+            //{
+            //    Console.WriteLine(n3 + "is greater number:");
+            //}
+            #endregion
+            if (n1 > n2 && n1 > n3)
+            {
+                Console.WriteLine(n1 + "is greater number:");
+            }
+            else
+            {
+                if (n2 > n3)
+                {
+                    Console.WriteLine(n2 + "is greater number:");
+                }
+                else
+                {
+                    Console.WriteLine(n3 + "is greater number:");
+                }
+            }
+
         }
-        
+
     }
 }
 
